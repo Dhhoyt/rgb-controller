e845f75094a7adb71758fa0550704ef10dde63e6
Dallin Hoyt <dhhoyt2002@gmail.com>
2023-03-31 10:22:51 -0600

    slider bar and passing variables

---
+++ lighting_effects.py
@@ -0,0 +1,19 @@
+def off():
+    pass
+
+def rainbow():
+    pass
+
+effects = {
+    "off": {
+        "function": off,
+        "display": "Off",
+        "controls": [],
+    },
+    "rainbow": {
+        "function": rainbow,
+        "display": "Rainbow",
+        "controls": ["SPEED"],
+    }
+}
+
--- rgbserver.py
+++ rgbserver.py
@@ -3,23 +3,32 @@
 from flask import Flask, render_template, jsonify, request
 from threading import Thread
 import time
+import lighting_effects as le
+import json
+
+current_effect = 0
 
 app = Flask(__name__)
 
+effects_list = dict(le.effects)
+
+for i in effects_list:
+    effects_list[i].pop('function', None)
+
+print(json.dumps(effects_list))
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    print(le.effects.keys())
+    return render_template("index.html", len = len(le.effects), effects_keys = list(le.effects.keys()), effects = le.effects, effects_string = effects_list)
 
 @app.route("/set_effect", methods=['POST'])
 def set_effect():
-    effect_json = request.get_json()
-    print(effect_json)
-    print(effect_json['effect'])
+    effect_json = request.get_json()    
     return '',204
 
 def start_lights():
     while True:
-        print("showing light effect")
         time.sleep(1)
 
 server_thread = Thread(target = lambda: app.run(host="0.0.0.0", port="8080"))
