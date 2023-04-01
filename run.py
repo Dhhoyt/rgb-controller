b5553eebf387aa503fccab8f21d145db42a5df80
Dallin Hoyt <dhhoyt2002@gmail.com>
2023-04-01 17:40:54 -0600

    thread stuff

--- run.py
+++ run.py
@@ -3,9 +3,7 @@ from threading import Thread
 
 app = create_app()
 
-server_thread = Thread(target = lambda: app.run(host="0.0.0.0", port=8080))
-
 lighting_thread = Thread(target = lambda: lighting_effects.start_lighting())
-
-server_thread.start()
 lighting_thread.start()
+
+app.run(host="0.0.0.0", port=8080)
